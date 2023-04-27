4259321db166eb50aef6415d0e1231e01e313c89
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-27 18:24:56 -0400

    Create jinja2.py

---
+++ qually/helpers/jinja2.py
@@ -0,0 +1,28 @@
+import base64
+import io
+import qrcode
+
+from qually.__main__ import app
+
+@app.template_filter("app_config")
+def app_config(x):
+    return app.config.get(x)
+
+@app.template_filter('qrcode_img_data')
+def qrcode_filter(x):
+  
+    mem=io.BytesIO()
+    qr=qrcode.QRCode()
+    qr.add_data(x)
+    img=qr.make_image(
+        fill_color=f"#{app.config['COLOR_PRIMARY']}",
+        back_color="white",
+    )
+    img.save(
+        mem, 
+        format="PNG"
+    )
+    mem.seek(0)
+    
+    data=base64.b64encode(mem.read()).decode('ascii')
+    return f"data:image/png;base64,{data}"
