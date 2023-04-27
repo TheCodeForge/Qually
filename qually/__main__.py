05555ad84a565572aee27d172fe0f0ec730b1c9f
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-27 16:16:57 -0400

    Update __main__.py

--- qually/__main__.py
+++ qually/__main__.py
@@ -8,6 +8,7 @@ from flask_caching import Cache
 from flask_limiter import Limiter
 from flask_minify import Minify
 import time
+from qually.helpers.security import generate_hash
 
 from flaskext.markdown import Markdown
 from sqlalchemy.ext.declarative import declarative_base
