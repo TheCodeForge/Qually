955a770b9bd1817b3513de50bf88d235e6f312e9
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-27 15:40:08 -0400

    Update organization.py

--- qually/classes/organization.py
+++ qually/classes/organization.py
@@ -10,10 +10,14 @@ class Organization(Base, core_mixin):
 
     __tablename__="organizations"
 
+    #basic info
     id = Column(Integer, primary_key=True)
     name = Column(String, default=None)
     license_count = Column(Integer, default=0)
+    license_expire_utc = Column(Integer, default=0)
 
     created_utc = Column(Integer, default=0)
     creation_ip = Column(String, default=None)
 
+    #organization settings
+    is_mfa_required = Column(Boolean, default=False)
