48f4596dde1e4fa61b0c681167b9e2417264704f
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-29 09:44:26 -0400

    Update route_imports.py

--- qually/helpers/route_imports.py
+++ qually/helpers/route_imports.py
@@ -1,7 +1,7 @@
 from flask import g, render_template, redirect, abort, session, jsonify, send_file, redirect
 
 from qually.classes import *
-from qually.helpers.wrappers import auth_required, is_active, user_update_lock, no_cors, no_sanctions cf_cache
+from qually.helpers.wrappers import auth_required, is_active, user_update_lock, no_cors, no_sanctions, cf_cache
 from qually.helpers.security import generate_hash, validate_hash
 
 from qually.__main__ import app, debug, limiter, debug, cache
