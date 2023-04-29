116af707738a459f5eaa1967f6fb9b0cbeb785a7
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-29 10:39:48 -0400

    Update logo.py

--- qually/routes/logo.py
+++ qually/routes/logo.py
@@ -5,205 +5,9 @@ from io import BytesIO
 
 from qually.helpers.route_imports import *
 
-@app.get(app.config["IMG_URL_LOGO_MAIN"])
-@cf_cache
-#@cache.memoize()
-def get_logo_main():
-
-    primary_r=int(app.config["COLOR_PRIMARY"][0:2], 16)
-    primary_g=int(app.config["COLOR_PRIMARY"][2:4], 16)
-    primary_b=int(app.config["COLOR_PRIMARY"][4:6], 16)
-
-    primary = (primary_r, primary_g, primary_b, 255)
-
-    base_layer = PIL.Image.open(f"{app.config['RUQQUSPATH']}/assets/images/logo/logo_base.png")
-    text_layer = PIL.Image.new("RGBA", base_layer.size, color=(255,255,255,0))
-
-    #flood fill main logo shape
-    ImageDraw.floodfill(
-        base_layer,
-        (base_layer.size[0]//2, base_layer.size[1]//2),
-        value=primary
-        )
-
-
-    #tilted letter layer
-    font = ImageFont.truetype(
-        f"{app.config['RUQQUSPATH']}/assets/fonts/Arial-bold.ttf", 
-        size=int(min(base_layer.size)//2)
-    )
-
-    letter = app.config["SITE_NAME"][0:1].lower()
-    box = font.getbbox(letter)
-
-    d = ImageDraw.Draw(text_layer)
-    d.text(
-        (
-            base_layer.size[0] // 2 - box[2] // 2, 
-            base_layer.size[0] // 2 - (box[3]+box[1]) // 2
-            ),
-        letter, 
-        font=font,
-        fill=(255,255,255,255)
-        )
-
-    text_layer = text_layer.rotate(
-        angle=20, 
-        expand=False, 
-        fillcolor=(255,255,255,0),
-        center=(
-            text_layer.size[0]//2,
-            text_layer.size[0]//2
-            ),
-        resample=PIL.Image.BILINEAR)
-
-    output=PIL.Image.alpha_composite(base_layer, text_layer)
-
-    bytesout=BytesIO()
-    output.save(bytesout, format="PNG")
-    bytesout.seek(0)
-    return send_file(bytesout, mimetype="image/png")
-
-
-@app.get(app.config["IMG_URL_LOGO_WHITE"])
-@cf_cache
-#@cache.memoize()
-def get_logo_white():
-
-    primary_r=int(app.config["COLOR_PRIMARY"][0:2], 16)
-    primary_g=int(app.config["COLOR_PRIMARY"][2:4], 16)
-    primary_b=int(app.config["COLOR_PRIMARY"][4:6], 16)
-
-    primary = (primary_r, primary_g, primary_b, 255)
-
-    base_layer = PIL.Image.open(f"{app.config['RUQQUSPATH']}/assets/images/logo/logo_base.png")
-    text_layer = PIL.Image.new("RGBA", base_layer.size, color=(255,255,255,0))
-
-    #tilted letter layer
-    font = ImageFont.truetype(
-        f"{app.config['RUQQUSPATH']}/assets/fonts/Arial-bold.ttf", 
-        size=int(min(base_layer.size)//2)
-    )
-
-    letter = app.config["SITE_NAME"][0:1].lower()
-    box = font.getbbox(letter)
-
-    d = ImageDraw.Draw(text_layer)
-    d.text(
-        (
-            base_layer.size[0] // 2 - box[2] // 2, 
-            base_layer.size[0] // 2 - (box[3]+box[1]) // 2
-            ),
-        letter, 
-        font=font,
-        fill=primary
-        )
-
-    text_layer = text_layer.rotate(
-        angle=20, 
-        expand=False, 
-        fillcolor=(255,255,255,0),
-        center=(
-            text_layer.size[0]//2,
-            text_layer.size[0]//2
-            ),
-        resample=PIL.Image.BILINEAR)
-
-    output=PIL.Image.alpha_composite(base_layer, text_layer)
-
-
-    bytesout=BytesIO()
-    output.save(bytesout, format="PNG")
-    bytesout.seek(0)
-    return send_file(bytesout, mimetype="image/png")
-
-@app.get(f"/logo/<kind>/{app.config['COLOR_PRIMARY'].lower()}/{app.config['SITE_NAME'][0].lower()}/<width>/<height>")
-@cf_cache
-#@cache.memoize()
-def get_assets_images_splash(kind, width, height, color=None, letter=None):
-
-    try:
-        width=int(width)
-        height=int(height)
-    except:
-        abort(404)
-
-    if max(width, height)>4500:
-        abort(404)
-
-    if kind not in ["splash", "thumb"]:
-        abort(404)
-
-    color = color or app.config["COLOR_PRIMARY"]
-
-    primary_r=int(color[0:2], 16)
-    primary_g=int(color[2:4], 16)
-    primary_b=int(color[4:6], 16)
-
-    primary = (primary_r, primary_g, primary_b, 255)
-
-    base_layer = PIL.Image.new("RGBA", (width, height), color=primary)
-
-    text_layer = PIL.Image.new("RGBA", (width, height), color=(255,255,255,0))
-
-    size=int(min(height, width)//1.2)
-
-    font = ImageFont.truetype(
-        f"{app.config['RUQQUSPATH']}/assets/fonts/Arial-bold.ttf", 
-        size=size
-        )
-
-    letter = app.config["SITE_NAME"][0:1].lower()
-    box = font.getbbox(letter)
-
-    d = ImageDraw.Draw(text_layer)
-    d.text(
-        (
-            width // 2 - box[2] // 2, 
-            height // 2 - (box[3]+box[1]) // 2
-            ),
-        letter, 
-        font=font,
-        fill=(255,255,255,255)
-        )
-
-    text_layer = text_layer.rotate(
-        angle=20, 
-        expand=False, 
-        fillcolor=primary,
-        resample=PIL.Image.BILINEAR)
-
-    if kind=="thumb":
-        font = ImageFont.truetype(
-            f"{app.config['RUQQUSPATH']}/assets/fonts/Arial-bold.ttf", 
-            size=int(size*0.6)
-            )
-        fullbox=font.getbbox(app.config["SITE_NAME"].lower())
-
-        if fullbox[2]>width:
-            abort(400)
-
-        d = ImageDraw.Draw(text_layer)
-        d.text(
-            (
-                width//2 - fullbox[2]//2,
-                height//2 + (box[3]-box[1]) // 2 + int(size*0.2)
-                ),
-            app.config["SITE_NAME"].lower(),
-            font=font,
-            fill=(255,255,255,255)
-            )
-
-    output=PIL.Image.alpha_composite(base_layer, text_layer)
-
-    bytesout=BytesIO()
-    output.save(bytesout, format="PNG")
-    bytesout.seek(0)
-    return send_file(bytesout, mimetype="image/png")
-
-@app.get("/logo/fontawesome/<style>/<icon>")
-@app.get("/logo/fontawesome/<style>/<icon>/<color>")
-@app.get("/logo/fontawesome/<style>/<icon>/<color>/<size>")
+@app.get("/icon/fontawesome/<style>/<icon>")
+@app.get("/icon/fontawesome/<style>/<icon>/<color>")
+@app.get("/icon/fontawesome/<style>/<icon>/<color>/<size>")
 @cf_cache
 def logo_fontawesome_icon(style, icon, color=None, size=500):
 
@@ -262,18 +66,13 @@ def logo_fontawesome_icon(style, icon, color=None, size=500):
     bytesout.seek(0)
     return send_file(bytesout, mimetype="image/png")
 
-@app.get("/favicon.ico")
-@cf_cache
-def get_favicon_ico():
-    return get_assets_images_splash("splash", 48, 48)
+# @app.get("/apple-touch-icon.png")
+# @app.get("/apple-touch-icon-precomposed.png")
+# @app.get("/apple-touch-icon-<width>x<height>-precomposed.png")
+# @app.get("/apple-touch-icon-<width>x<height>.png")
+# @cf_cache
+# def get_apple_touch_icon_sized_png(width=192, height=192):
 
-@app.get("/apple-touch-icon.png")
-@app.get("/apple-touch-icon-precomposed.png")
-@app.get("/apple-touch-icon-<width>x<height>-precomposed.png")
-@app.get("/apple-touch-icon-<width>x<height>.png")
-@cf_cache
-def get_apple_touch_icon_sized_png(width=192, height=192):
-
-    width=int(width)
-    height=int(height)
-    return get_assets_images_splash('splash', width, height)
+#     width=int(width)
+#     height=int(height)
+#     return get_assets_images_splash('splash', width, height)
