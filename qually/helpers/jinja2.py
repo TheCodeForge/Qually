9a0a56efae8adfac51308bf7c04c69966b0b4111
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-29 13:58:59 -0400

    Update jinja2.py

--- qually/helpers/jinja2.py
+++ qually/helpers/jinja2.py
@@ -2,6 +2,7 @@ from flask import g, session
 import base64
 import io
 import qrcode
+import pyotp
 
 from qually.helpers.security import generate_hash
 
@@ -14,6 +15,31 @@ def app_config(x):
 @app.template_filter("csrf_token")
 def logged_out_formkey(t):
     return generate_hash(f"{t}+{session['session_id']}")
+
+@app.template_filter('otp_qrcode')
+def qrcode_filter(secret):
+  
+    mem=io.BytesIO()
+    qr=qrcode.QRCode(qrcode.constants.ERROR_CORRECT_L)
+
+    x=pyotp.TOTP(secret)
+
+    qr.add_data(x.provisioning_uri(g.user.username, issuer_name=app.config["SITE_NAME"]))
+
+    img=qr.make_image(
+        fill_color=f"#{app.config['COLOR_PRIMARY']}",
+        back_color="white",
+    )
+    
+    img.save(
+        mem, 
+        format="PNG"
+    )
+    mem.seek(0)
+    
+    data=base64.b64encode(mem.read()).decode('ascii')
+    return f"data:image/png;base64,{data}"
+
 
 @app.template_filter('qrcode_img_data')
 def qrcode_filter(x):
