952f81e152d6689066b8f142454282d423554649
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-30 08:43:02 -0400

    Update settings.py

--- qually/routes/settings.py
+++ qually/routes/settings.py
@@ -9,3 +9,17 @@ def get_settings_profile():
 
     page=request.path.split("/")[2]
     return render_template(f"settings/{page}.html")
+
+@app.post("/settings/organization")
+@is_admin
+def post_settings_organization():
+
+
+    if request.form.get("org_name"):
+
+        g.user.organization.name=request.form.get("org_name")
+        g.db.add(g.user.organization)
+
+    g.db.commit()
+
+    return toast("Organization settings saved!")
