7e4b024610424efe1818e5966cbd37c5ebca5d1c
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-30 00:55:32 -0400

    Update settings.py

--- qually/routes/settings.py
+++ qually/routes/settings.py
@@ -1,6 +1,11 @@
 from qually.helpers.route_imports import *
 
 @app.get("/settings/profile")
+@app.get("/settings/security")
+@app.get("/settings/organization")
+@app.get("/settings/directory")
 @logged_in
-def get_settings_profile(page):
-    return render_template("settings/profile.html")
+def get_settings_profile():
+
+    page=request.path.split("/")[2]
+    return render_template(f"settings/{page}.html")
