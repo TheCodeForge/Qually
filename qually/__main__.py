9cbe043d96757366ac91fbf2e10ac7f971ebe45c
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-30 11:43:49 -0400

    Update __main__.py

--- qually/__main__.py
+++ qually/__main__.py
@@ -185,6 +185,9 @@ def before_request():
         user = g.db.query(User).options(joinedload(User.organization)).filter_by(id=session.get("user_id")).first()
         if user.is_active and user.login_nonce == session.get("login_nonce", 0):
             g.user = user
+        else:
+            session.pop("user_id")
+            session.pop("login_nonce")
 
     #for non-idempotent requests, check csrf token
     if request.method in ["POST", "PUT", "PATCH", "DELETE"] and request.url_rule:
