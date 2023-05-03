49cd52895fbe8543acd84badd22e3de6d905d00a
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-03 13:25:17 -0400

    Create mail.py

---
+++ qually/helpers/mail.py
@@ -0,0 +1,30 @@
+import requests
+
+def send_mail(to_address, subject, html, plaintext=None, files={},
+              from_address=f"{app.config['SITE_NAME']} <noreply@mail.{app.config['SERVER_NAME']}>"):
+
+    if not environ.get("MAILGUN_KEY"):
+        debug("Cannot send mail - no mailgun key")
+        return
+
+
+    url = f"https://api.mailgun.net/v3/mail.{app.config['SERVER_NAME']}/messages"
+
+    data = {"from": from_address,
+            "to": [to_address],
+            "subject": subject,
+            "text": plaintext,
+            "html": html,
+            }
+
+    x= requests.post(
+        url,
+        auth=(
+            "api", environ.get("MAILGUN_KEY").lstrip().rstrip()
+            ),
+        data=data,
+        files=[("attachment", (k, files[k])) for k in files]
+        )
+
+   # debug([g.user.username, url, x.status_code, x.content])
+    return x
