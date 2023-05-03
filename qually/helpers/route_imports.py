23d2e78018861501892c12bfdd277920f563b4b7
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-03 12:01:15 -0400

    Update route_imports.py

--- qually/helpers/route_imports.py
+++ qually/helpers/route_imports.py
@@ -4,7 +4,7 @@ from werkzeug.security import safe_join
 
 from qually.classes import *
 from qually.helpers.wrappers import logged_in, not_logged_in, has_seat, is_admin, user_update_lock, org_update_lock, no_cors, no_sanctions, cf_cache, token_auth
-from qually.helpers.security import generate_hash, validate_hash, tokenify, verify_token
+from qually.helpers.security import generate_hash, validate_hash, tokenify
 from qually.helpers.posttoast import toast, toast_redirect, toast_error
 from qually.helpers.get import *
 
