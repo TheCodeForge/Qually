ec1d86abb37110da5bc992ecef6a4c6bc5c757f3
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-05 15:37:31 -0400

    Update org_settings.py

--- qually/routes/org_settings.py
+++ qually/routes/org_settings.py
@@ -191,7 +191,7 @@ def post_settings_plan():
     if new_seat_count < g.user.organization.licenses_used:
         return toast_error(f"You can't reduce your organization license count below current usage.")
 
-    if g.time < g.user.organization.licenses_last_increased_utc + 60*60*24*21:
+    if new_seat_count < g.user_organization.license_count and g.time < g.user.organization.licenses_last_increased_utc + 60*60*24*21:
         return toast_error("There is a 21 day cooldown after increasing license count before it may be decreased.")
         
     #compute remaining seatseconds
