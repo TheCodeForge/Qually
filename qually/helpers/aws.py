e40e31c74b43b1224924ac333482f0446745e0d8
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-06 13:54:05 -0400

    Update aws.py

--- qually/helpers/aws.py
+++ qually/helpers/aws.py
@@ -11,11 +11,35 @@ S3 = boto3.client(
 	aws_secret_access_key=app.config["AWS_SECRET_ACCESS_KEY"]
 	)
 
-def upload_file(name, file):
+def crop_and_resize(img, resize):
+
+    i = img
+
+    # get constraining dimension
+    org_ratio = i.width / i.height
+    new_ratio = resize[0] / resize[1]
+
+    if new_ratio > org_ratio:
+        crop_height = int(i.width / new_ratio)
+        box = (0, (i.height // 2) - (crop_height // 2),
+               i.width, (i.height // 2) + (crop_height // 2))
+    else:
+        crop_width = int(new_ratio * i.height)
+        box = ((i.width // 2) - (crop_width // 2), 0,
+               (i.width // 2) + (crop_width // 2), i.height)
+
+    return i.resize(resize, box=box)
+
+def upload_file(name, file resize=None):
 
 	tempname=name.replace("/","_")
 
 	file.save(tempname)
+
+	if resize:
+        i = Image.open(tempname)
+        i = crop_and_resize(i, resize)
+        i.save(tempname)
 
 	S3.upload_file(
 		tempname,
