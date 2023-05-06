195ce772ba30ff035e77910cca2ea2a36e1f4006
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-06 13:38:32 -0400

    Update class_imports.py

--- qually/helpers/class_imports.py
+++ qually/helpers/class_imports.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import deferred, relationship
 from qually.helpers.lazy import lazy
 from qually.helpers.base36 import base36encode, base36decode
 from qually.helpers.security import generate_hash, validate_hash
+import qually.helpers.aws as aws
 
 from qually.classes.mixins import core_mixin
 
