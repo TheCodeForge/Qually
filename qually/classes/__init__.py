c99b23bca428952e279fe50dbbbc6ff7a2d2c909
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-13 19:17:18 -0400

    Update __init__.py

--- qually/classes/__init__.py
+++ qually/classes/__init__.py
@@ -1,4 +1,4 @@
-from .ncmr import NCMR
+from .ncmr import NCMR, NCMRLog
 from .organization import Organization, OrganizationAuditLog
 from .paypal import PayPalClient, PayPalTxn
 from .user import User
