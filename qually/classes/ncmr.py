5a1b7e5bc2bf37302c4b495b25fc4359dc105179
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-14 08:35:50 -0400

    Update ncmr.py

--- qually/classes/ncmr.py
+++ qually/classes/ncmr.py
@@ -25,8 +25,7 @@ class NCMR(Base, core_mixin):
     __table_args__=(
         UniqueConstraint(
             'number', 
-            'organization_id', 
-            name='ncmr_org_number_unique'),
+            'organization_id', name='ncmr_org_number_unique'),
         )
 
     @property
