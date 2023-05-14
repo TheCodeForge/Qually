e575d7d23e99a2ff0e0272c0653162f5558ec9be
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-14 19:35:34 -0400

    Update ncmr.py

--- qually/routes/ncmr.py
+++ qually/routes/ncmr.py
@@ -18,24 +18,25 @@ def post_ncmr_number(number):
 
     ncmr = get_ncmr(number)
 
-    entries=ncmr._layout[ncmr._status]
+    with force_locale(g.user.organization.lang):
+        entries=ncmr._layout[ncmr._status]
 
     for entry in entries:
         if entry['value'] in request.form:
             if entry['kind']=='multi':
                 setattr(ncmr, entry['raw'], request.form[entry['value']])
                 setattr(ncmr, entry['value'], html(request.form[entry['value']]))
-                key=entry['value']
+                key=entry['name']
                 value=txt(request.form[entry['value']])
                 response=getattr(ncmr, entry['value'])
             elif entry['kind']=='dropdown':
                 setattr(ncmr, entry['value'], int(request.form[entry['value']]))
-                key=entry['value']
+                key=entry['name']
                 value=entry['values'].get(int(request.form[entry['value']]))
                 response=value
             else:
                 setattr(ncmr, entry['value'], txt(request.form[entry['value']]))
-                key=entry['value']
+                key=entry['name']
                 value=getattr(ncmr, entry['value'])
                 response=value
 
