81fa9f0f219f68ed12f715f9de492e9d107da3a0
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-14 18:17:46 -0400

    dropdown

--- qually/classes/ncmr.py
+++ qually/classes/ncmr.py
@@ -18,12 +18,24 @@ class NCMR(Base, core_mixin):
     owner = relationship("User")
     logs = relationship("NCMRLog", order_by="NCMRLog.id.desc()")
 
-    ##process data
-    item_number=Column(String)
-    lot_number=Column(String)
-    quantity=Column(String)
-    nc_description=Column(String)
-    nc_description_raw=Column(String)
+    ##New
+    item_number=Column(String, default="")
+    lot_number=Column(String, default="")
+    quantity=Column(String, default="")
+    nc_description=Column(String, default="")
+    nc_description_raw=Column(String), default=""
+    new_comments=Column(String, default="")
+    new_comments_raw =Column(String, default="")
+
+    ##MRB
+    _disposition_determined=Column(Integer, default=None)
+    mrb_comments=Column(String, default="")
+    mrb_comments_raw =Column(String, default="")
+
+    ##MRB
+    _disposition_actual=Column(Integer, default=None)
+    dsp_comments=Column(String, default="")
+    dsp_comments_raw =Column(String, default="")
 
 
     __table_args__=(
@@ -50,9 +62,21 @@ class NCMR(Base, core_mixin):
             1: _("Submitted"),
             2: _("Material Review Board"),
             3: _("Disposition"),
-            4: _("Closed"),
+            4: _("Final Review"),
+            5: _("Closed"),
             100: _("Terminated")
         }
+
+    @property
+    def _dispositions(self):
+        return {
+            0: _("Scrap"),
+            1: _("Return to Supplier"),
+            2: _("Rework"),
+            3: _("Use As-Is"),
+            4: _("Reclassify")
+        }
+    
     
 
     @property
@@ -84,6 +108,40 @@ class NCMR(Base, core_mixin):
                 "value":"nc_description",
                 "kind": "multi",
                 "raw": "nc_description_raw"
+                },
+                {
+                "name":_("Additional Comments"),
+                "value":"new_comments",
+                "kind": "multi",
+                "raw": "new_comments_raw"
+                }
+            ],
+            3:[
+                {
+                "name":_("Assigned Disposition"),
+                "value":"_disposition_determined",
+                "kind": "dropdown",
+                "values": self._dispositions
+                },
+                {
+                "name":_("Material Review Board Comments"),
+                "value":"mrb_comments",
+                "kind": "multi",
+                "raw": "mrb_comments_raw"
+                }
+            ],
+            4:[
+                {
+                "name":_("Executed Disposition"),
+                "value":"_disposition_actual",
+                "kind": "dropdown",
+                "values": self._dispositions
+                },
+                {
+                "name":_("Additional Comments"),
+                "value":"dsp_comments",
+                "kind": "multi",
+                "raw": "dsp_comments_raw"
                 }
             ]
         }
