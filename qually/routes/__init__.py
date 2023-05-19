951c85b633eb16d58dbd77372c1caa6f3d9c7cd7
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-18 22:20:41 -0400

    record

--- qually/routes/__init__.py
+++ qually/routes/__init__.py
@@ -2,8 +2,8 @@ from .errors import *
 from .front import *
 from .login import *
 from .logo import *
-from .ncmr import *
 from .org_settings import *
+from .record import *
 from .settings import *
 from .static import *
 from .users import *
