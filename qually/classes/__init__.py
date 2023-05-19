093f833f0af13bbd73c5866086874d2151d56542
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-19 16:54:46 -0400

    Update __init__.py

--- qually/classes/__init__.py
+++ qually/classes/__init__.py
@@ -1,3 +1,4 @@
+from .capa import CAPA, CAPAApproval, CAPALog
 from .ncmr import NCMR, NCMRApproval, NCMRLog
 from .organization import Organization, OrganizationAuditLog
 from .paypal import PayPalClient, PayPalTxn
