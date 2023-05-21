d1c6f8fcc53604f682f0527bc780a3cfa8c9dc28
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-21 19:21:45 -0400

    Update organization.py

--- qually/classes/organization.py
+++ qually/classes/organization.py
@@ -41,6 +41,7 @@ class Organization(Base, core_mixin):
     #relationships
     _users=relationship("User", lazy="dynamic")
     logs=relationship("OrganizationAuditLog", lazy="dynamic", order_by="OrganizationAuditLog.id.desc()")
+    
     ncmrs=relationship("NCMR", lazy="dynamic", viewonly=True)
     capas=relationship("CAPA", lazy="dynamic", viewonly=True)
 
