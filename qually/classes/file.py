e2912e912069c2e59bfe58bf2e7b2e88b2790ff6
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-25 21:22:46 -0400

    Update file.py

--- qually/classes/file.py
+++ qually/classes/file.py
@@ -16,7 +16,7 @@ class File(Base, core_mixin):
     #connection IDs
     ncmr_id = Column(Integer, ForeignKey("ncmr.id"))
     capa_id = Column(Integer, ForeignKey("capa.id"))
-    dvtn_id = Column(Integer, ForeignKey("deviation.id"))
+    dvtn_id = Column(Integer, ForeignKey("dvtn.id"))
 
     ncmr=relationship("NCMR", lazy="joined", backref="files")
     capa=relationship("CAPA", lazy="joined", backref="files")
