fe00ff9ee5d1dc9f3307cdcd3656c3109bdda0c3
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-29 17:23:59 -0400

    item

--- qually/classes/__init__.py
+++ qually/classes/__init__.py
@@ -1,6 +1,7 @@
 from .capa import CAPA, CAPAApproval, CAPALog
 from .deviation import Deviation, DeviationApproval, DeviationLog
 from .file import File
+from .item import Item, ItemRevision
 from .ncmr import NCMR, NCMRApproval, NCMRLog
 from .organization import Organization, OrganizationAuditLog
 from .paypal import PayPalClient, PayPalTxn
---
+++ qually/classes/item.py
@@ -0,0 +1,70 @@
+from qually.helpers.class_imports import *
+try:
+    from flask_babel import gettext as _
+except ModuleNotFoundError:
+    def _(x):
+        return x
+
+
+class Item(Base, core_mixin):
+
+    __tablename__="item"
+
+    id=Column(Integer, primary_key=True)
+    created_utc=Column(Integer)
+
+    revisions=relationship("ItemRevision", lazy="dynamic", order_by="ItemRevision.id.desc()")
+
+    @property
+    def _lifecycle(self):
+        return {
+            0: {
+                'name': _("Draft"),
+                'users': [self.owner],
+                'files': True
+                },
+            1: {
+                'name': _("Design"),
+                'users': self.organization.doc_control_users
+                },
+            2: {
+                'name': _("Production"),
+                'users': self.organization.mrb_users
+                },
+            100: {
+                'name': _("Obsolete"),
+                'users': [self.assignee],
+                'files': True
+                }
+        }
+
+    @property
+    def current_revision(self):
+        return self.revisions.filter_by(_status=1).first()
+    
+
+class ItemRevision(Base, core_mixin):
+
+    __tablename__="itemrevision"
+
+    id=Column(Integer, primary_key=True)
+    created_utc=Column(Integer)
+    item_id=Column(Integer, ForeignKey(Item.id))
+    file_id=Column(Integer, ForeignKey("files.id"))
+
+    _status=Column(Integer, default=0)
+
+
+    @property
+    def _lifecycle(self):
+        return {
+            0:_("Proposed"),
+            1:_("Effective"),
+            2:_("Superceded")
+        }
+
+    @property
+    def status(self):
+        return self._lifecycle[self._status]
+    
+
