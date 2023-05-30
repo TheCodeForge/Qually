7c7272ab0d29d4431ba070f31880212057138e59
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-30 19:10:14 -0400

    Update __init__.py

--- qually/classes/__init__.py
+++ qually/classes/__init__.py
@@ -1,12 +1,12 @@
 from .capa import CAPA, CAPAApproval, CAPALog
 from .deviation import Deviation, DeviationApproval, DeviationLog
 from .file import File
-from .item import Item, ItemRevision
+from .item import Item, ItemRevision, ItemRelationship
 from .ncmr import NCMR, NCMRApproval, NCMRLog
 from .organization import Organization, OrganizationAuditLog
 from .paypal import PayPalClient, PayPalTxn
 from .user import User
 
 ALL_PROCESSES={
-    x._name.lower(): x for x in [CAPA, Deviation, NCMR]
+    x._name.lower(): x for x in [CAPA, Deviation, NCMR, Item]
 }
