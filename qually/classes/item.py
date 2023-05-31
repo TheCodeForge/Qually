f36fff7558b17c322e402815d6024d0e75ccbeb0
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-05-30 20:31:55 -0400

    Update item.py

--- qually/classes/item.py
+++ qually/classes/item.py
@@ -14,6 +14,7 @@ class Item(Base, core_mixin, process_mixin):
     _kind_id=Column(Integer, default=1)
     owner_id=Column(Integer, ForeignKey("users.id"))
     created_utc=Column(BigInteger)
+    organization_id = Column(Integer, ForeignKey("organizations.id"))
 
     revisions=relationship("ItemRevision", lazy="dynamic", order_by="ItemRevision.id.desc()")
 
