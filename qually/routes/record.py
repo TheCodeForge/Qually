ad0281b78cd0177762070a07410afcae2f87c437
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-06-08 21:53:36 -0400

    Update record.py

--- qually/routes/record.py
+++ qually/routes/record.py
@@ -20,6 +20,8 @@ def get_record_number(kind, number, rev=None):
     elif request.path != record.display_revision.permalink:
         return redirect(record.display_revision.permalink)
     
+    print app._lifecycle
+
     return render_template("record.html", record=record)
 
 @app.post("/<kind>-<number>")
