ea24e442929c3a2cb94546051b92660c468ae2df
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-06-08 21:45:01 -0400

    Update record.py

--- qually/routes/record.py
+++ qually/routes/record.py
@@ -17,7 +17,7 @@ def get_record_number(kind, number, rev=None):
 
     if request.path != record.permalink and not rev:
         return redirect(record.permalink)
-    elif request.path != rev.display_revision.permalink:
+    elif request.path != record.display_revision.permalink:
         return redirect(rev.permalink)
     
     return render_template("record.html", record=record)
