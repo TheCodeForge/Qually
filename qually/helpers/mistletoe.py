169fd712c149a3b34535ce611b9a4b6760dc1014
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-06-10 18:13:55 -0400

    Update mistletoe.py

--- qually/helpers/mistletoe.py
+++ qually/helpers/mistletoe.py
@@ -4,14 +4,16 @@ from mistletoe.block_token import BlockToken
 from mistletoe.span_token import SpanToken
 from mistletoe.html_renderer import HTMLRenderer
 
+from qually.classes import ALL_PROCESSES
+
 
 class RecordMention(SpanToken):
-    pattern = re.compile(r"\w{4}-(\d{5,})")
+    pattern = re.compile(r"\w{2,5}-(\d{5,})")
     parse_inner = False
-    precedence=-1
+    precedence=100
 
     def __init__(self, match_obj):
-        self.target=match_obj.group(0).upper()
+        self.target=match_obj.group(0)
 
 class CustomRenderer(HTMLRenderer):
 
@@ -21,5 +23,9 @@ class CustomRenderer(HTMLRenderer):
             )
 
     def render_record_mention(self, token):
+
+        prefix=token.split('-')[0]
+
+        if prefix.lower() not in 
 
         return f'<a href="/{token.target}">{token.target}</a>'
