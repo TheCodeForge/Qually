b894407b80fe55c2ec1fe48c07b71477563f2494
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-06-15 09:54:28 -0400

    Update static.py

--- qually/routes/static.py
+++ qually/routes/static.py
@@ -153,7 +153,7 @@ def get_s3_object_path(oid, fid, path):
     #create the stamp canvas
     packet=io.BytesIO()
     can=canvas.Canvas(packet, pagesize=letter)
-    can.drawString(36,36, stamp_text, "1 0 0")
+    can.drawString(36,36, stamp_text, color="1 0 0")
     can.save()
     packet.seek(0)
     stamp_pdf=pypdf.PdfReader(packet)
