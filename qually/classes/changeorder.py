02eba9fc5b270d1edfd0fda5e67bb9061905f0d7
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-06-27 17:40:55 -0400

    Update changeorder.py

--- qually/classes/changeorder.py
+++ qually/classes/changeorder.py
@@ -416,7 +416,7 @@ class ChangeOrder(Base, core_mixin, process_mixin):
             existing.status_utc=g.time
             g.db.add(rev)
 
-            rev.item._status = rev.item._status or 2
+            rev.item._status = rev.revision_item_lifecycle_to
             g.db.add(rev.item)
 
         g.db.commit()
